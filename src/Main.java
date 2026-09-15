19dda9dc8d5e7c8d25d863fc00d7dbbb32af941c
Dox0225 <128Mguttman@frhsd.com>
2026-09-15 13:49:48 -0400

    Errors fixed

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
     }
-}
-public String longestWord(ArrayList<String> list){
-    String longestWord=list.get(0);
-    for(int i=1;i<list.size();i++){
-        if(list.get(i).length()>longestWord.length()){
-            longestWord=list.get(i);
+
+    public String longestWord(ArrayList<String> list) {
+        String longestWord = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).length() > longestWord.length()) {
+                longestWord = list.get(i);
+            }
         }
+        return longestWord;
     }
-    return longestWord;
 }
