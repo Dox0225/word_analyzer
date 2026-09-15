433b6a48bbee46cfd153711192f38e410c111bec
Dox0225 <128Mguttman@frhsd.com>
2026-09-15 13:59:25 -0400

    or statements in findVowels

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
     public String findVowels(ArrayList<String> list) {
         for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).indexOf(0).equals('a')) {
+            if(list.get(i).charAt(0)==('a')|| list.get(i).charAt(0)==('e')||list.get(i).charAt(0)==('i')||list.get(i).charAt(0)==('o')||list.get(i).charAt(0)==('i')){
                 return list.get(i);
             }
         }
