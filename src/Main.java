29c3bc2d0540c349df28206d5edbed6a6e030501
Dox0225 <128Mguttman@frhsd.com>
2026-09-15 13:45:29 -0400

    Longest method is done

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class word_analyzer {
 }
 public String longestWord(ArrayList<String> list){
     String longestWord=list.get(0);
+    for(int i=1;i<list.size();i++){
+        if(list.get(i).length()>longestWord.length()){
+            longestWord=list.get(i);
+        }
+    }
     return longestWord;
 }
