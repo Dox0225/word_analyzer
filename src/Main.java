38bb86e3e95a96c34404dbc2006ab8619253e41a
Dox0225 <128Mguttman@frhsd.com>
2026-09-15 13:43:12 -0400

    Longest method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class word_analyzer {
     ArrayList <String> list = new ArrayList<>();
 }
-public String Longest(){
-    return;
+public String longestWord(ArrayList<String> list){
+    String longestWord=list.get(0);
+    return longestWord;
 }
