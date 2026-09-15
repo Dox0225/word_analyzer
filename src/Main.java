ab4a774dbd251b8267ab280402c5e4d557d8269c
Dox0225 <128Mguttman@frhsd.com>
2026-09-15 13:35:51 -0400

    ArrayList changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) {
-        ArrayList <String> list = new ArrayList();
-    }
+public class word_analyzer {
+    ArrayList <String> list = new ArrayList<>();
+}
+public String Longest(){
+    return;
 }
